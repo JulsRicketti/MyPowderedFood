ee0e4d44178c5c5fbb0cd0e5d762b59efef341aa
Julia Ricketti <jricketti@launchfort.com>
2020-08-02 16:36:48 -0400

    First commit

---
+++ src/data/powderedFood.js
@@ -0,0 +1,150 @@
+export const powderedFood = [
+  {
+    id: 1,
+    brand: 'Queal',
+    calories: 424,
+    fat: {
+      total: 15.5,
+      saturated: 1.7
+    },
+    carbohydrates: {
+      total: 49.2,
+      sugars: 13.9
+    },
+    protein: 18.7,
+    fibre: 7.2,
+    salt: 0.7,
+    vitaminsAndMinerals: {
+      vitaminA: {
+        quantity: 152,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminD: {
+        quantity: 3.03,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminE: {
+        quantity: 2.63,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminK: {
+        quantity: 1.41,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminC: {
+        quantity: 22.2,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      thiamine: {
+        quantity: 0.27,
+        unit: 'mg',
+        dailyPercentage: 23
+      },
+      riboflavine: {
+        quantity: 0.32,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      niacine: {
+        quantity: 3.23,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      vitaminB6: {
+        quantity: 0.34,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      folicAcid: {
+        quantity: 51,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminB12: {
+        quantity: 0.81,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      biotine: {
+        quantity: 8.08,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      vitaminB5: {
+        quantity: 1.01,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      potassium: {
+        quantity: 0.71,
+        unit: 'g',
+        dailyPercentage: 20
+      },
+      chloride: {
+        quantity: 0.46,
+        unit: 'g',
+        dailyPercentage: 20
+      },
+      calcium: {
+        quantity: 0.19,
+        unit: 'g',
+        dailyPercentage: 26
+      },
+      phosphorus: {
+        quantity: 199,
+        unit: 'mg',
+        dailyPercentage: 36
+      },
+      magnesium: {
+        quantity: 71,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      iron: {
+        quantity: 2.22,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      zinc: {
+        quantity: 3.29,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      copper: {
+        quantity: 0.3,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+      manganese: {
+        quantity: 1.6,
+        unit: 'mg',
+        dailyPercentage: 54
+      },
+      selenium: {
+        quantity: 14.1,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      molybdenum: {
+        quantity: 13.1,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      iodine: {
+        quantity: 30.3,
+        unit: 'μg',
+        dailyPercentage: 20
+      },
+      choline: {
+        quantity: 81,
+        unit: 'mg',
+        dailyPercentage: 20
+      },
+    }
+  }
+]
