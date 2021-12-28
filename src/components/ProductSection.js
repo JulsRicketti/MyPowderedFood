02f4009ab71fcf9ac969e120ae354b793c4a273f
Julia Ricketti <jricketti@launchfort.com>
2021-12-28 16:53:19 -0500

    finish implementing the forms for the product section

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react'
 import { ProductContext } from '../context/ProductContext'
 import { Form, Input, Select, Divider } from 'antd'
-import { PlusOutlined } from '@ant-design/icons'
 
 export default function ProductSection () {
   const { powderedFood, brands } = useContext(ProductContext)
@@ -12,6 +11,12 @@ export default function ProductSection () {
   const [note, setNote] = useState('')
   const [calories, setCalories] = useState(0)
   const [price, setPrice] = useState(0)
+  const [currency, setCurrency] = useState('')
+  const [servings, setServings] = useState('')
+
+  const foodByBrand = brand ? powderedFood.find(f => f.brand === brand) : ''
+  const productWebsite = foodByBrand && foodByBrand.site
+  console.warn(foodByBrand)
   return (
     <div>
       <h3>Section 1- Product</h3>
@@ -44,22 +49,45 @@ export default function ProductSection () {
           </Select>
         </Form.Item>
         <Form.Item label='Product name'>
-
+          <Input
+            value={productName}
+            onChange={(evt) => setProductName(evt.target.value)}
+          />
         </Form.Item>
         <Form.Item label='Website'>
-
+          <Input
+            disabled={!!productWebsite}
+            value={productWebsite || website}
+            onChange={(evt) => setWebsite(evt.target.value)}
+          />
         </Form.Item>
         <Form.Item label='Note'>
-
+          <Input.TextArea
+            value={note}
+            onChange={(evt) => setNote(evt.target.value)}
+          />
         </Form.Item>
-        <Form.Item label='Calories'>
-
+        <Form.Item label='Calories per serving'>
+          <Input
+            type='number'
+            value={calories}
+            onChange={(evt) => setCalories(evt.target.value)}
+          />
         </Form.Item>
         <Form.Item label='Full Price'>
-
+          <Input
+            type='number'
+            value={price}
+            onChange={(evt) => setPrice(evt.target.value)}
+          />
+          <Select/>
         </Form.Item>
         <Form.Item label='Servings'>
-
+          <Input
+            type='number'
+            value={servings}
+            onChange={(evt) => setServings(evt.target.value)}
+          />
         </Form.Item>
       </Form>
 
