dc1ea93c1dec69a232899671533c9093a867aa19
Julia Ricketti <jricketti@launchfort.com>
2021-12-30 13:22:16 -0500

    button improvements

--- src/components/FoodEditorModal.js
+++ src/components/FoodEditorModal.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Modal } from 'antd'
+import { Modal, Button } from 'antd'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons'
 import ProductSection from './ProductSection'
 
+const sections = ['product', 'macronutrients', 'vitaminsAndMinerals']
+
 export default function FoodEditorModal ({ visible }) {
+  // 1- product, 2- macronutrients, 3- vitaminsAndMinerals
+  const [section, setSection] = useState(0)
+  const buttons = (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Button
+        disabled={!section}
+        onClick={() => setSection(section - 1)}
+        icon={<ArrowLeftOutlined />}
+      />
+      <Button
+        disabled={section === sections.length - 1}
+        onClick={() => setSection(section + 1)}
+        icon={<ArrowRightOutlined />}
+      />
+    </div>
+  )
   return (
     <Modal title="Add food" visible={true}>
       <ProductSection/>
+      {buttons}
     </Modal>
   )
 }
